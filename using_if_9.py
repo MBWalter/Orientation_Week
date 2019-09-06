95cf188a570f88c6db3bb3ae4ab42e4bf6cd7185
MBWalter <54991386+MBWalter@users.noreply.github.com>
2019-09-06 23:06:17 +0200

    Create using_if_9.py

---
+++ using_if_9.py
@@ -0,0 +1,7 @@
+a = int(input("Enter 'a' : "))
+b = int(input("Enter 'b' : "))
+c = int(input("Enter 'c' : "))
+if (c * c == a * a + b * b):
+    print("the 3 number can make a triangle :)")
+else:
+    print("the 3 number can't make a triangle :(")
