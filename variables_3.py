a03f06ccb4e9b335ddebdaf55394c964cb1cc05f
MBWalter <54991386+MBWalter@users.noreply.github.com>
2019-09-06 22:22:01 +0200

    Create variables_3.py

---
+++ variables_3.py
@@ -0,0 +1,7 @@
+number1 = input("Which is the first natural number: ")
+number2 = input("Which is the second natural number: ")
+print("")
+quotient = int(number1) // int(number2)
+remnant = int(number1) % int(number2)
+print("The quotient :" , quotient)
+print("The remnant: " , remnant)
